4ef38364fdfb0c56c2e6028259eb1cc98b9941bd
AyushiTripathi234 <ayushitripathi12081@gmail.com>
2026-09-21 23:53:50 +0530

    Time: 163 ms (50.89%) | Memory: 151.2 MB (55.36%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        
+        vector<long long> ans(k, 0);
+
+        vector<long long> dp(k, 0);
+
+        for (int num : nums) {
+            vector<long long> next(k, 0);
+
+            int rem = num % k;
+            next[rem]++;
+
+            for (int r = 0; r < k; r++) {
+                if (dp[r] == 0) continue;
+
+                int newRem = (long long)r * rem % k;
+                next[newRem] += dp[r];
+            }
+
+                    for (int r = 0; r < k; r++) {
+                ans[r] += next[r];
+            }
+
+            dp = move(next);
+        }
+
+        return ans;
+    }
+};
